e1da6136a3141161eb10ab1a5a586bfe6c63ff32
Zubov Sergey <serj@zsa-studio.ru>
2023-12-16 16:55:19 +0300

    test coommit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <imask-input
+      v-model="numberModel"
+      :mask="Number"
+      radix="."
+      :unmask="true"
+      @accept="onAccept"
+      placeholder='Enter number here'
+    />
+  </div>
+</template>
+
+<script>
+import { IMaskComponent } from 'vue-imask';
+export default {
+  name: 'IndexPage',
+
+  data () {
+    return {
+      numberModel: '',
+    }
+  },
+  components: {
+    'imask-input': IMaskComponent
+  },
+  methods: {
+    onAccept (value) {
+      console.log(value);
+    }
+  }
+}
+</script>
